b02fbe1c9594eb210435e49946c770f5acfb58cf
Arjun8464 <71544080+Arjun8464@users.noreply.github.com>
2021-12-16 20:56:30 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,15 @@
+function Bedroom(){
+    window.location="bedroom.html";
+}
+function TV_AC(){
+    window.location="TVAC.html";
+}
+function desk(){
+    window.location="desk.html";
+}
+function Bottles(){
+    window.location="bottles.html";
+}
+function Fruit_basket(){
+    window.location="fruit_basket.html";
+}
